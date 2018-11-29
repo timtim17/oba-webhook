4da31a43fb565dc59c04fbeb66e0cd133283e410
Austin Jenchi <ajenchi9@gmail.com>
2018-11-29 01:45:28 -0800

    test

--- OBAAPIConnection.py
+++ OBAAPIConnection.py
@@ -19,7 +19,7 @@ class OBAAPIConnection:
         for key in params.keys():
             param_str += "&%s=%s" % (key, params[key])
         
-        path = _OBA_PREFIX + func + ".json?key=" + self.apiKey + param_str + "&time=2018-11-29_13-00-00"
+        path = _OBA_PREFIX + func + ".json?key=" + self.apiKey + param_str
         print("API Request: %s" % path)
 
         return requests.get(_OBA_URL + path).json()
@@ -28,7 +28,7 @@ class OBAAPIConnection:
         return self._call_func(_OBA_FUNCTIONS['stops_for_location'], {"lat": location['latitude'], "lon": location['longitude'], "maxCount": max_count})
     
     def trips_for_route(self, route_id, include_status=False, include_schedule=False):
-        return self._call_func(_OBA_FUNCTIONS['trips_for_route'], {'includeStatus': include_status, 'includeSchedule': include_schedule})
+        return self._call_func(_OBA_FUNCTIONS['trips_for_route'], {'includeStatus': include_status, 'includeSchedule': include_schedule, 'time': '2018-11-29_13-00-00'})
     
     def nearby_routes(self, location, max_count=None, lat_span=0.05, lon_span=0.05):
         params = {"lat": location['latitude'], "lon": location['longitude'], "latSpan": lat_span, "lonSpan": lon_span}
