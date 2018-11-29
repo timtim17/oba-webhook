37b4ef7868a5e8752ccad9eff69e394f5d511351
Austin Jenchi <ajenchi9@gmail.com>
2018-11-28 20:33:55 -0800

    fix syntax

--- main.py
+++ main.py
@@ -61,9 +61,8 @@ def nearby_routes(location):
     if len(routes) == 0:
         return {'fulfillmentText': "Sorry, there seems to be no routes running near you right now."}
     else:
-        routes_as_str = [((r['shortName'] + ": " + r['description']) if r['description'] else r['longName']) for r in routes if r['description']
-        resp_text = "The nearest routes right now are "
-                        + _list_to_str(routes_as_str)
+        routes_as_str = [((r['shortName'] + ": " + r['description']) if r['description'] else r['longName']) for r in routes if r['description']]
+        resp_text = "The nearest routes right now are " + _list_to_str(routes_as_str)
         return {
             'fulfillmentText': resp_text,
             'payload': {
